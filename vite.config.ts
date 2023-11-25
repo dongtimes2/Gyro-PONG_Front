ed5d4f81ab7dde3f533e1f3aedab5616e2f91d7a
dongtimes2 <dongtimes2@gmail.com>
2023-11-25 21:03:49 +0900

    feat: vite로 마이그레이션 및 typescript 도입

---
+++ vite.config.ts
@@ -0,0 +1,18 @@
+/*eslint-disable*/
+/// <reference types="vitest" />
+/// <reference types="vite/client" />
+
+import react from '@vitejs/plugin-react-swc';
+import { defineConfig } from 'vite';
+import tsconfigPaths from 'vite-tsconfig-paths';
+
+export default defineConfig({
+  plugins: [react(), tsconfigPaths()],
+  server: {
+    port: 3000,
+    open: true,
+  },
+  build: {
+    outDir: './build',
+  },
+});
