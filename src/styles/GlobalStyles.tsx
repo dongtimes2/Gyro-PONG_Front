bd7a2b8a7dc69c297d6a76a60a4d6a472a8e2f4c
dongtimes2 <dongtimes2@gmail.com>
2023-12-10 19:55:49 +0900

    feat: 가로모드 및 사이즈 감지 기능 추가

--- src/styles/GlobalStyles.tsx
+++ src/styles/GlobalStyles.tsx
@@ -46,17 +46,13 @@ const GlobalStyles = createGlobalStyle`
   h6 {
     overflow-wrap: break-word;
   }
-  #root,
-  #__next {
-    isolation: isolate;
-  }
 
   /* font */
   @font-face {
-  font-family: 'DungGeunMo';
-  src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_six@1.2/DungGeunMo.woff') format('woff');
-  font-weight: normal;
-  font-style: normal;
+    font-family: 'DungGeunMo';
+    src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_six@1.2/DungGeunMo.woff') format('woff');
+    font-weight: normal;
+    font-style: normal;
   }
 
   body {
@@ -65,6 +61,49 @@ const GlobalStyles = createGlobalStyle`
     font-family: 'DungGeunMo';
     user-select: none;
     overflow: hidden;
+  }
+
+  @media screen and (orientation: portrait) {
+    .mobile:not(:has(div.motionButtonArea)) {
+      &::before {
+        content: '가로모드로 전환해주세요';
+        position: fixed;
+        top: 0;
+        bottom: 0;
+        left: 0;
+        right: 0;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        width: 100vw;
+        height: 100%;
+        background-color: ${({ theme }) => theme.colors.black};
+        z-index: 999;
+        user-select: none;
+      }
+    }
+  }
+
+  @media all and (max-width: 1280px), (max-height: 800px) {
+    .pc {
+      &::before {
+        content: '더 넓은 사이즈의 화면에서 이용해주세요.';
+        position: fixed;
+        top: 0;
+        bottom: 0;
+        left: 0;
+        right: 0;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        width: 100vw;
+        height: 100%;
+        font-size: 2rem;
+        background-color: ${({ theme }) => theme.colors.black};
+        z-index: 999;
+        user-select: none;
+      }
+    }
   }
 `;
 
