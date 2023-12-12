cf30a5d80f2efc71a14fc8d7cc88f57bdd12d301
dongtimes2 <dongtimes2@gmail.com>
2023-12-13 05:00:39 +0900

    feat: 컨트롤러 연동하지 않은 유저가 lobby, game 페이지로의 접속을 막음

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from 'react-router-dom';
+import { Navigate, Outlet, Route, Routes } from 'react-router-dom';
 
 import Controller from '@pages/Controller/Controller';
 import Game from '@pages/Game/Game';
@@ -11,6 +11,17 @@ import Settings from '@pages/Settings/Settings';
 import { PATH } from './constants/path';
 import ControllerRoute from './routes/ControllerRoute';
 import PcRoute from './routes/PcRoute';
+import { useUserStore } from './store/user';
+
+const PrivateRoute = () => {
+  const controllerSocketId = useUserStore((state) => state.controllerSocketId);
+
+  if (controllerSocketId) {
+    return <Outlet />;
+  } else {
+    return <Navigate to={PATH.HOME} />;
+  }
+};
 
 const App = () => {
   return (
@@ -19,8 +30,10 @@ const App = () => {
         <Route path={PATH.HOME} element={<Home />} />
         <Route path={PATH.GUIDES} element={<Guides />} />
         <Route path={PATH.SETTINGS} element={<Settings />} />
-        <Route path={PATH.LOBBY} element={<Lobby />} />
-        <Route path={PATH.GAME} element={<Game />} />
+        <Route element={<PrivateRoute />}>
+          <Route path={PATH.LOBBY} element={<Lobby />} />
+          <Route path={PATH.GAME} element={<Game />} />
+        </Route>
       </Route>
       <Route element={<ControllerRoute />}>
         <Route path={PATH.CONTROLLER} element={<Controller />} />
